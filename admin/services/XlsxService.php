eb786bf151270e1be84b1a6df2914ce94d46248f
hariachmad <hari08achmad@gmail.com>
2025-03-21 04:38:32 +0700

    REMOVE BUG XLS SERVICE

--- admin/services/XlsxService.php
+++ admin/services/XlsxService.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once (__DIR__."/../../vendor/autoload.php"    );
+require_once(__DIR__ . "/../../vendor/autoload.php");
 require_once(__DIR__ . "/../models/ListPengajuanModel.php");
 
 use PhpOffice\PhpSpreadsheet\Worksheet\Drawing;
@@ -27,6 +27,10 @@ class XlsxService
         $sheet->getColumnDimension('G')->setWidth(10);
         $sheet->getColumnDimension('H')->setWidth(20);
         $sheet->getColumnDimension('I')->setWidth(22);
+        $sheet->getColumnDimension('J')->setWidth(22);
+        $sheet->getColumnDimension('K')->setWidth(22);
+        $sheet->getColumnDimension('L')->setWidth(22);
+        $sheet->getColumnDimension('M')->setWidth(22);
         $sheet->getColumnDimension('N')->setWidth(22);
         $headersAfterSplit = explode(",", $headers);
 
@@ -36,6 +40,7 @@ class XlsxService
 
         if ($result["pengajuanLembur"]->num_rows > 0) {
             $rowNumber = 2;
+            $fotoPengajuan = $result["fotoPengajuanBefore"]->fetch_all(MYSQLI_ASSOC);
             while ($row = $result["pengajuanLembur"]->fetch_assoc()) {
                 $sheet->setCellValue([1, $rowNumber], $row['nama_karyawan']);
                 $sheet->setCellValue([2, $rowNumber], $row['jenis_proyek']);
@@ -46,34 +51,40 @@ class XlsxService
                 $sheet->setCellValue([7, $rowNumber], $row['status_pengajuan']);
                 $sheet->setCellValue([8, $rowNumber], isset($row['approver_role']) ? $row['approver_role'] : '-');
 
-                $colsFotoSebelum = ['I','J','K','L','M'];
-                $indexColsFotoSebelum=0;
-                while ($row = $result["fotoPengajuanBefore"]->fetch_assoc()) {
-                    $drawing = new Drawing();
-                    $drawing->setName('Foto'); 
-                    $drawing->setDescription('Foto');
-                    $drawing->setPath('user/'.$row["path"]);
-                    $drawing->setCoordinates($colsFotoSebelum[$indexColsFotoSebelum].$rowNumber);
-                    $drawing->setWidth(10); 
-                    $drawing->setHeight(10); 
-                    $drawing->setWorksheet($sheet); 
-                    $indexColsFotoSebelum= $indexColsFotoSebelum+ 1;
+                $colsFotoSebelum = ['I', 'J', 'K', 'L', 'M'];
+                $indexColsFotoSebelum = 0;
+                foreach($fotoPengajuan as $rowFoto){
+                    if ($row["pengajuan_id"] == $rowFoto["pengajuan_id"]) {
+                        $drawing = new Drawing();
+                        $drawing->setName('Foto');
+                        $drawing->setDescription('Foto');
+                        $drawing->setPath('user/' . $rowFoto["path"]);
+                        $drawing->setCoordinates($colsFotoSebelum[$indexColsFotoSebelum] . $rowNumber);
+                        $drawing->setWidth(10);
+                        $drawing->setHeight(20);
+                        $drawing->setWorksheet($sheet);
+                        $indexColsFotoSebelum = $indexColsFotoSebelum + 1;
+                    }
                 }
+                $rowNumber = $rowNumber + 1;
+                // while ($rowFoto = $result["fotoPengajuanBefore"]->fetch_assoc()) {
+                    
+                // }
 
-                $colsFotoSesudah = ['N','O','P','Q','R'];
-                $indexColsFotoSesudah=0;
-                while ($row = $result["fotoPengajuanAfter"]->fetch_assoc()) {
-                    $drawing = new Drawing();
-                    $drawing->setName('Foto');
-                    $drawing->setDescription('Foto');
-                    $drawing->setPath('user/'.$row["path"]);
-                    $drawing->setCoordinates($colsFotoSesudah[$indexColsFotoSesudah].$rowNumber);
-                    $drawing->setWidth(10); 
-                    $drawing->setHeight(10); 
-                    $drawing->setWorksheet($sheet); 
-                    $indexColsFotoSesudah= $indexColsFotoSesudah+ 1;
-                }
-                $rowNumber += 1;
+                // $colsFotoSesudah = ['N', 'O', 'P', 'Q', 'R'];
+                // $indexColsFotoSesudah = 0;
+                // while ($row = $result["fotoPengajuanAfter"]->fetch_assoc()) {
+                //     $drawing = new Drawing();
+                //     $drawing->setName('Foto');
+                //     $drawing->setDescription('Foto');
+                //     $drawing->setPath('user/' . $row["path"]);
+                //     $drawing->setCoordinates($colsFotoSesudah[$indexColsFotoSesudah] . $rowNumber);
+                //     $drawing->setWidth(10);
+                //     $drawing->setHeight(10);
+                //     $drawing->setWorksheet($sheet);
+                //     $indexColsFotoSesudah = $indexColsFotoSesudah + 1;
+                // }
+
             }
         }
 
